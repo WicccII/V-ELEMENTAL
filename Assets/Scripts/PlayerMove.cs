4f499006fbd9c51762beb80791c5fa37ec4eee52
DeWick <Ducsieuda@gmail.com>
2024-08-28 00:49:54 +0700

    test

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,6 +6,7 @@ public class PlayerMove : MonoBehaviour
 {
     public float moveSpeed;
     Rigidbody2D myRigidbody2D;
+    Vector2 moveDirection;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +16,23 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        InputManagaer();
+    }
 
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void InputManagaer()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        moveDirection = new Vector2(x, y).normalized;
+    }
+    private void Move()
+    {
+        myRigidbody2D.velocity = new Vector2(moveDirection.x * moveSpeed * Time.deltaTime, moveDirection.y * moveSpeed * Time.deltaTime);
     }
 }
