7e3206afa96d0ff8278d5bc116efccb9612f0e3e
Nguyễn Văn Khởi <khoinv.it@gmail.com>
2024-10-19 11:36:50 +0700

    fix-setting-audio

--- Assets/VolumeSettings.cs
+++ Assets/VolumeSettings.cs
@@ -10,22 +10,49 @@ public class VolumeSettings : MonoBehaviour
     [SerializeField] private Slider masterSlider;
     [SerializeField] private Slider musicSlider;
     [SerializeField] private Slider SFXSlider;
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("masterVolume") && PlayerPrefs.HasKey("musicVolume") && PlayerPrefs.HasKey("SFXVolume"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+            SetMasterVolume();
+            SetMusicVolume();
+            SetSFXVolume();
+        }
+    }
 
     public void SetMasterVolume()
     {
         float volume = masterSlider.value;
-        myMixer.SetFloat("master", volume);
+        myMixer.SetFloat("master", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("masterVolume", volume);
     }
 
     public void SetMusicVolume()
     {
         float volume = musicSlider.value;
-        myMixer.SetFloat("music", volume);
+        myMixer.SetFloat("music", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
     }
+
     public void SetSFXVolume()
     {
         float volume = SFXSlider.value;
-        myMixer.SetFloat("SFX", volume);
+        myMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("SFXVolume", volume);
     }
 
+    private void LoadVolume()
+    {
+        masterSlider.value = PlayerPrefs.GetFloat("masterVolume");
+        musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        SFXSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+
+        SetMasterVolume();
+        SetMusicVolume();
+        SetSFXVolume();
+    }
 }
