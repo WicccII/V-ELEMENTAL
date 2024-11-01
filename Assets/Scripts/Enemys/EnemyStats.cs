4070caec8bed9cc64bb0b41afce370623a9e2652
DeWick <Ducsieuda@gmail.com>
2024-11-01 15:44:51 +0700

    Update EnemyStats.cs

--- Assets/Scripts/Enemys/EnemyStats.cs
+++ Assets/Scripts/Enemys/EnemyStats.cs
@@ -1,7 +1,5 @@
 using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
-using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 using Image = UnityEngine.UI.Image;
 
